16132bd4bc06bf86186c922531e7066656409ba1
Miller <miller@fake.org>
2022-05-21 19:40:28 +0700

    Add push and pop (back)

--- src/vec/vec.hpp
+++ src/vec/vec.hpp
@@ -1,27 +1,62 @@
 #pragma once
 
 #include <cstring>
+#include <stdexcept>
 
-template <typename T, size_t InlineCap>
+namespace vec {
+
+template <typename T, size_t inline_cap>
 class SmallVector {
  private:
-  char initial[sizeof(T) * InlineCap] = {0};
+  char initial[sizeof(T) * inline_cap] = {0};
   size_t size = 0;
-  size_t capacity = InlineCap;
+  size_t capacity = inline_cap;
+  size_t heap_cap = 0;
+  T* data = nullptr;
 
  public:
   SmallVector(){};
+  ~SmallVector() { delete[] data; };
 
-  void push(T elem) {
-    if (size < capacity) {
+  size_t Size() const { return size; }
+  size_t Capacity() const { return capacity; }
+  size_t InlineCapacity() const { return inline_cap; }
+
+  void Push(T elem) {
+    if (size < inline_cap) {
       std::memcpy(&initial[sizeof(T) * size], &elem, sizeof(T));
-      size += 1;
+    } else {
+      if (data == nullptr) {
+        capacity *= 2;
+        data = new T[capacity];
+      } else if (size == capacity) {
+        capacity *= 2;
+        T* old = data;
+        data = new T[capacity];
+        std::memcpy(data, old, sizeof(T) * size);
+        delete[] old;
+      }
+      data[size - inline_cap] = elem;
+    }
+    size += 1;
+  }
+
+  void Pop() {
+    if (size == 0) {
+      return;
+    }
+    size -= 1;
+  }
+
+  const T& operator[](size_t index) const {
+    if (index < inline_cap) {
+      return (T&)(initial[sizeof(T) * index]);
+    } else if (index < size) {
+      return data[size - inline_cap - 1];
+    } else {
+      throw std::out_of_range("Small vector out of bounds");
     }
   }
-
-  T& operator[](size_t index) { return (T&)(initial[sizeof(T) * index]); }
-
-  const T* initial_begin() const {
-    return reinterpret_cast<const T*>(&initial[0]);
-  }
 };
+
+}  // namespace vec
--- test/test_vec.cpp
+++ test/test_vec.cpp
@@ -1,11 +1,35 @@
 #include <vec/vec.hpp>
 
 #include <gtest/gtest.h>
+namespace vec {
 
-TEST(SMALL_VEC, PUSH) {
+TEST(SMALL_VEC, WITHIN_INLINE_CAP) {
   SmallVector<int, 4> four;
-  four.push(1);
-  ASSERT_EQ(1, four[0]);
-  four.push(2);
-  ASSERT_EQ(2, four[1]);
+  four.Push(0);
+  ASSERT_EQ(0, four[0]);
+  four.Push(1);
+  ASSERT_EQ(1, four[1]);
 }
+
+TEST(SMALL_VEC, INCREASE_CAP) {
+  SmallVector<int, 2> two;
+  two.Push(0);
+  two.Push(1);
+
+  ASSERT_EQ(2,two.Size());
+  ASSERT_EQ(2,two.Capacity());
+
+  two.Push(2);
+  ASSERT_EQ(2, two[2]);
+
+  ASSERT_EQ(3,two.Size());
+  ASSERT_EQ(4,two.Capacity());
+
+  two.Push(3);
+  two.Push(4);
+
+  ASSERT_EQ(5,two.Size());
+  ASSERT_EQ(8,two.Capacity());
+}
+
+}  // namespace vec
